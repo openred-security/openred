6b4374662322b2e5bd74da5a6c37d94b7ee3a64e
openred <161236304+theopenred@users.noreply.github.com>
2024-04-26 19:54:22 +0200

    feature(build): Docker building process

--- actions/build.sh
+++ actions/build.sh
@@ -2,7 +2,12 @@
 
 chmod +x gradlew
 #Generate binary tarballs at build/distribution/local/opensearch-*-SNAPSHOT/
+#./gradlew localDistro
 ./gradlew localDistro
+tar -czvf opensearch-2.13.0.tar.gz -C distribution/archives/linux-tar/build/install/opensearch-2.13.0-SNAPSHOT/ actions/docker/release
+#Generate docker image (use opensearch-build repository)
+cd actions/docker/release/
+sudo ./build-image-single-arch.sh -v 2.13.0 -f ./dockerfiles/opensearch.al2.dockerfile -p opensearch -a x64 -t opensearch-2.13.0.tar.gz
 
 #Compress if required
 #tar -cvf opensearch-2.6.0-SNAPSHOT.tar.gz opensearch-2.6.0-SNAPSHOT/
